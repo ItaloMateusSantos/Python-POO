53cf8659b326a15d8fdfc566fc773a4daf02d0a4
ItaloMateusSantos <160606669+ItaloMateusSantos@users.noreply.github.com>
2024-09-03 14:28:00 +0000

    Aula02

---
+++ Atividades (POO-PYTHON)/Atividade 002.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+import os
+os.system('cls || clear')
+
+class Funcionario(ABC):
+    def __init__(self, nome: str, email: str, salario: float, endereco: Endereco) -> None:
+        self.nome = nome
+        self.email = email
+        self.salario = salario
+        self.endereco = endereco
+
+    @abstractmethod
+    def salario_final(self) -> float:
+        pass
+
+    def Endereco(Funcionario):
+        def salario_final(selfe) -> float:
+            
