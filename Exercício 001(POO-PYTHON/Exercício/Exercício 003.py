9559ba34f1a00127b80be8aef39759fd899d352d
ItaloMateusSantos <160606669+ItaloMateusSantos@users.noreply.github.com>
2024-09-03 11:50:20 +0000

    feat:  Aula 02

--- Exercício 001(POO-PYTHON/Exercício/Exercício 003.py
+++ Exercício 001(POO-PYTHON/Exercício/Exercício 003.py
@@ -11,9 +11,8 @@ class Funcionario(ABC):
         self.endereco = endereco
 
     @abstractmethod
-    def salario_final(self) -> float:
+    def calcular_salario(self) -> float:
         pass
-
 
 class Endereco(Funcionario):
     def calcular_salario(self) -> float:
@@ -24,7 +23,31 @@ class Endereco(Funcionario):
         cidade: str
 
 class Engenheiro(Funcionario):
+    def calcular_salario(self) -> float:
+         BONIFICACAO = 1.2 #Equivalente a 20% de alguma coisa. 
+         salario_final = self.salario * BONIFICACAO
+         return salario_final
 
 class Medico(Funcionario):
+    def calcular_salario(self) -> float:
+        DESCONTO = 0.2
+        salario_final = self.salario * DESCONTO
+        return salario_final
 
+print('-----INFORMAÇÕES DO ENGENHIRO------')
+engenheiro1 = Engenheiro('Miguel', 25, 3000.0, 'miguel@123', 'Rua 35 n° 10')
+print(f'Nome: {engenheiro1.nome}')
+print(f'Idade: {engenheiro1.idade}')
+print(f'Salário com bonificação: {engenheiro1.calcular_salario()}')
+print(f'Email: {engenheiro1.email}')
+print(f'Endereço: {engenheiro1.endereco}')
 
+print('\n')
+
+print('-----INFORMAÇÕES DO MÉDICO-----')
+medico1 = Medico('Ana', 25, 3500.0, 'ana@1000', 'rua 35 nº 11')
+print(f'Nome: {medico1.nome}')
+print(f'Idade: {medico1.idade}')
+print(f'Salário com desconto: {medico1.calcular_salario()}')
+print(f'Email: {medico1.email}')
+print(f'Endereço: {medico1.endereco}')
